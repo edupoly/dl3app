c1e06e8b0d17255957b27b14f05ba90d813fe585
edupoly <info.edupoly@gmail.com>
2026-06-05 13:04:04 +0530

    jsx-styling

--- src/Practice.jsx
+++ src/Practice.jsx
@@ -1,34 +1,9 @@
 function Practice() {
-  var a = 120;
-  var b = 3;
-  var c = "Harisha";
-  var ar = ["deepika", "alia", "Sonakshi", "Katrina"];
-  var student = {
-    firstname: "Jyothi",
-    age: 20,
-    gender: "Female",
-  };
-
   return (
     <div className="mybox">
-      <h1>Lets understand JSX</h1>
-      <h2>{10 + 20}</h2>
-      <h2>{a + b}</h2>
-      <h2>{a * b}</h2>
-      <h2>{a ** b}</h2>
-      <h2>{c.toUpperCase()}</h2>
-      <ul>
-        <li>{ar[0]}</li>
-        <li>{ar[1]}</li>
-        <li>{ar[2]}</li>
-      </ul>
-      <ol>
-        {ar.map((h) => {
-          return <li>{h}</li>;
-        })}
-      </ol>
-      <h1>{student.firstname}</h1>
-      <h2>{student.gender}</h2>
+      <h1>Lets Understand Styling in JSX</h1>
+      <h1 style={{ color: "red", border: "2px solid blue" }}>Praveen</h1>
+      <h2 className="abcd">Rishikesh</h2>
     </div>
   );
 }
