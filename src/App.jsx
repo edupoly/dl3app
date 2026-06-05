ca86cadda23009a955238e07c798ff3814e6bfda
edupoly <info.edupoly@gmail.com>
2026-06-05 12:05:54 +0530

    basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,9 @@
 import "./index.css";
-import Second from "./Second";
-import Third from "./Thid";
-import Nalugu from "./Fourth";
 
 function App() {
   return (
     <div className="mybox">
       <h1>Welcome to Modern Application Development</h1>
-      <Second></Second>
-      <Third></Third>
-      <Nalugu></Nalugu>
     </div>
   );
 }
