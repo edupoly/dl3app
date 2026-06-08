73955dfccfb62c1a13b926beed97e7ae1c853a01
edupoly <info.edupoly@gmail.com>
2026-06-08 12:10:23 +0530

    jsx-eventhandling

--- src/Practice.jsx
+++ src/Practice.jsx
@@ -1,9 +1,50 @@
+import { useState } from "react";
+
 function Practice() {
+  function abc() {
+    alert("How are you doing???");
+  }
+  var a = 100;
+  var b = 200;
+  function adding() {
+    alert(a + b);
+  }
+  var [n, updateNfn] = useState("");
+  function xyz(ev) {
+    // var v = document.getElementById("d1").value;
+    // var v = ev.target.value;
+    updateNfn(ev.target.value);
+    document.getElementById("d2").innerText = v;
+  }
   return (
     <div className="mybox">
-      <h1>Lets Understand Styling in JSX</h1>
-      <h1 style={{ color: "red", border: "2px solid blue" }}>Praveen</h1>
-      <h2 className="abcd">Rishikesh</h2>
+      <h3>Practice Component</h3>
+      <button
+        onClick={() => {
+          abc();
+        }}
+      >
+        Say HI
+      </button>
+      <button
+        onClick={() => {
+          adding();
+        }}
+      >
+        Add
+      </button>
+      <br />
+      <br />
+      <input
+        type="text"
+        onKeyUp={(event) => {
+          xyz(event);
+        }}
+      />
+      <h1>
+        {/* Entered Text:<span id="d2"></span> */}
+        Entered Text:<span>{n}</span>
+      </h1>
     </div>
   );
 }
