b0c06cf8ddb91a328c4ca07f60be8e90a8e5efd4
edupoly <info.edupoly@gmail.com>
2026-06-16 12:08:14 +0530

    base

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,10 @@
 import "./index.css";
-import Second from "./Second";
-import MyContext from "./MyContext";
+
 function App() {
-  var a = 100;
   return (
-    <MyContext.Provider
-      value={{ name: "Praveen", age: 30, place: "Bangalore" }}
-    >
-      <div className="mybox">
-        <h1>Welcome to Modern Application Development {a}</h1>
-        <Second x={a}></Second>
-      </div>
-    </MyContext.Provider>
+    <div className="mybox">
+      <h1>Welcome to Modern Application Development</h1>
+    </div>
   );
 }
 
--- src/MyContext.js
+++
@@ -1,3 +0,0 @@
-import { createContext } from "react";
-var MyContext = createContext();
-export default MyContext;
