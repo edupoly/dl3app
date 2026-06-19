65ff07fe83a24e2f2a36117e038ea7c436ef5dd9
edupoly <info.edupoly@gmail.com>
2026-06-19 11:39:03 +0530

    routingbasics

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,17 @@
-import Counter from "./Counter";
-import Gallery from "./Gallery";
+import { Outlet, Link } from "react-router-dom";
 import "./index.css";
-import LikeDislike from "./LikeDislike";
-import Practice from "./Practice";
-import Todolist from "./Todolist";
 
 function App() {
   return (
     <div className="mybox">
-      <h1>Welcome to Component State</h1>
-      <Gallery></Gallery>
-      <Todolist></Todolist>
-      <LikeDislike></LikeDislike>
-      <Counter></Counter>
-      <Practice></Practice>
+      <h1>Welcome to App Component</h1>
+      <Link to="/">Home</Link>
+      &nbsp;&nbsp;&nbsp;
+      <Link to="/counter">Counter</Link>
+      &nbsp;&nbsp;&nbsp;
+      <Link to="/todolist">Todolist</Link>
+      &nbsp;&nbsp;&nbsp;
+      <Outlet></Outlet>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,25 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Counter from "./Counter.jsx";
+import Todolist from "./Todolist.jsx";
+var router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children: [
+      {
+        path: "/counter",
+        element: <Counter></Counter>,
+      },
+      {
+        path: "/todolist",
+        element: <Todolist></Todolist>,
+      },
+    ],
+  },
+]);
 
-createRoot(document.getElementById("root")).render(<App />);
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router}></RouterProvider>,
+);
